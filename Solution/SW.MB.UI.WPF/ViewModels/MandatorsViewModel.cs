59072cf2c297f2e5d2d1013e62d3f8e6d35a715d
Michael Schläpfer <schlaem91@gmail.com>
2022-10-14 15:25:16 +0200

    MasterDetailView for mandators implemented rudimental

--- Solution/SW.MB.UI.WPF/ViewModels/MandatorsViewModel.cs
+++ Solution/SW.MB.UI.WPF/ViewModels/MandatorsViewModel.cs
@@ -7,29 +7,36 @@ using SW.MB.Domain.Models.Records;
 using SW.MB.UI.WPF.Models.Observables;
 
 namespace SW.MB.UI.WPF.ViewModels {
-    public class MandatorsViewModel : ViewModelBase {
-        public ObservableCollection<ObservableMandator> Mandators { get; } = new();
+  public class MandatorsViewModel: ViewModelBase {
+    private ObservableMandator? _SelectedMandator;
 
-        #region CONSTRUCTORS
-        public MandatorsViewModel(IServiceProvider serviceProvider) : base(serviceProvider) {
-            LoadMandators();
-        }
-        #endregion CONSTRUCTORS
+    public ObservableCollection<ObservableMandator> Mandators { get; } = new();
 
-        private void LoadMandators() {
-            if (ServiceProvider.GetService<IMandatorsService>() is IMandatorsService service) {
-                Mandators.Clear();
-                foreach (MandatorRecord mandator in service.GetAll()) {
-                    Mandators.Add(new ObservableMandator(mandator));
-                }
-            }
-        }
-
-        private void StoreMandators() {
-            if (ServiceProvider.GetService<IMandatorsService>() is IMandatorsService service) {
-                service.UpdateRange(Mandators.Select(x => x.ToRecord()).ToArray());
-                LoadMandators();
-            }
-        }
+    public ObservableMandator? SelectedMandator {
+      get => _SelectedMandator;
+      set => SetProperty(ref _SelectedMandator, value);
     }
+
+    #region CONSTRUCTORS
+    public MandatorsViewModel(IServiceProvider serviceProvider) : base(serviceProvider) {
+      LoadMandators();
+    }
+    #endregion CONSTRUCTORS
+
+    private void LoadMandators() {
+      if (ServiceProvider.GetService<IMandatorsService>() is IMandatorsService service) {
+        Mandators.Clear();
+        foreach (MandatorRecord mandator in service.GetAll()) {
+          Mandators.Add(new ObservableMandator(mandator));
+        }
+      }
+    }
+
+    private void StoreMandators() {
+      if (ServiceProvider.GetService<IMandatorsService>() is IMandatorsService service) {
+        service.UpdateRange(Mandators.Select(x => x.ToRecord()).ToArray());
+        LoadMandators();
+      }
+    }
+  }
 }
