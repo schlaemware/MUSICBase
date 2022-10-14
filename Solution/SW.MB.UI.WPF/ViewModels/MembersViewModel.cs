881dac198b5c2789b0b1bb8104192e1142a85b26
Michael Schläpfer <schlaem91@gmail.com>
2022-10-14 15:14:35 +0200

    MasterDetailView for members implemented rudimental

--- Solution/SW.MB.UI.WPF/ViewModels/MembersViewModel.cs
+++ Solution/SW.MB.UI.WPF/ViewModels/MembersViewModel.cs
@@ -7,38 +7,49 @@ using SW.MB.Domain.Models.Records;
 using SW.MB.UI.WPF.Models.Observables;
 
 namespace SW.MB.UI.WPF.ViewModels {
-    public class MembersViewModel : ViewModelBase {
-        public ObservableCollection<ObservableMember> Members { get; } = new();
+  public class MembersViewModel: ViewModelBase {
+    private ObservableMember? _SelectedMember;
 
-        #region CONSTRUCTORS
-        public MembersViewModel(IServiceProvider serviceProvider) : base(serviceProvider) {
-            IMandatorsService.MandatorChanged += IMandatorsService_MandatorChanged;
-            
-            LoadMembers();
+    public ObservableCollection<ObservableMember> Members { get; } = new();
+
+    public ObservableMember? SelectedMember {
+      get => _SelectedMember;
+      set {
+        if (SetProperty(ref _SelectedMember, value)) {
+          System.Diagnostics.Debug.WriteLine($"Changed to {value?.Fullname ?? "NULL"}");
         }
-        #endregion CONSTRUCTORS
-
-        private void LoadMembers() {
-            if (ServiceProvider.GetService<IMembersService>() is IMembersService service) {
-                Members.Clear();
-
-                foreach (MemberRecord member in service.GetAll(ActiveMandator?.ToRecord())) {
-                    Members.Add(new ObservableMember(member));
-                }
-            }
-        }
-
-        private void StoreMembers() {
-            if (ServiceProvider.GetService<IMembersService>() is IMembersService service) {
-                service.UpdateRange(Members.Select(x => x.ToRecord()).ToArray());
-                LoadMembers();
-            }
-        }
-
-        #region CALLBACKS
-        private void IMandatorsService_MandatorChanged(object? sender, EventArgs e) {
-            LoadMembers();
-        }
-        #endregion CALLBACKS
+      }
     }
+
+    #region CONSTRUCTORS
+    public MembersViewModel(IServiceProvider serviceProvider) : base(serviceProvider) {
+      IMandatorsService.MandatorChanged += IMandatorsService_MandatorChanged;
+
+      LoadMembers();
+    }
+    #endregion CONSTRUCTORS
+
+    private void LoadMembers() {
+      if (ServiceProvider.GetService<IMembersService>() is IMembersService service) {
+        Members.Clear();
+
+        foreach (MemberRecord member in service.GetAll(ActiveMandator?.ToRecord())) {
+          Members.Add(new ObservableMember(member));
+        }
+      }
+    }
+
+    private void StoreMembers() {
+      if (ServiceProvider.GetService<IMembersService>() is IMembersService service) {
+        service.UpdateRange(Members.Select(x => x.ToRecord()).ToArray());
+        LoadMembers();
+      }
+    }
+
+    #region CALLBACKS
+    private void IMandatorsService_MandatorChanged(object? sender, EventArgs e) {
+      LoadMembers();
+    }
+    #endregion CALLBACKS
+  }
 }
