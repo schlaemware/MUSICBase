9da1ccab3013b1499d6d63cde27e60e2d1f3be41
Michael Schläpfer <schlaem91@gmail.com>
2022-11-24 08:22:46 +0100

    Started winUI3 project.

---
+++ Solution/SW.MB.UI.WinUI3/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+using Microsoft.UI.Xaml;
+
+namespace SW.MB.UI.WinUI3.Views.Windows {
+  /// <summary>
+  /// An empty window that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainWindow: Window {
+    public MainWindow() {
+      this.InitializeComponent();
+    }
+
+    private void myButton_Click(object sender, RoutedEventArgs e) {
+      myButton.Content = "Clicked!";
+    }
+  }
+}
